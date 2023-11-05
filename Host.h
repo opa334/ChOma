37a2fa8c51f1f56bbec91654c035dac7371e4e50
opa334 <opa334@users.noreply.github.com>
2023-11-05 14:09:19 +0100

    Improve comment

--- Host.h
+++ Host.h
@@ -4,6 +4,7 @@
 #include "MachOContainer.h"
 
 // Retrieve the preferred MachO slice from a MachO container
+// Preferred slice as in the slice that the kernel would use when loading the file
 MachO *macho_container_find_preferred_macho_slice(MachOContainer *machoContainer);
 
 #endif // HOST_H
