f4fd1bb0d44bbc71f86461feb33155f1f2ec8adc
opa334 <opa334@users.noreply.github.com>
2024-01-20 22:46:18 +0100

    Export this

--- src/Host.h
+++ src/Host.h
@@ -3,6 +3,8 @@
 
 #include "FAT.h"
 
+int host_get_cpu_information(cpu_type_t *cputype, cpu_subtype_t *cpusubtype);
+
 // Retrieve the preferred MachO slice from a FAT
 // Preferred slice as in the slice that the kernel would use when loading the file
 MachO *fat_find_preferred_slice(FAT *fat);
