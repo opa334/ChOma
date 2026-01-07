opa334/ChOma

commit 77c2a6d0da0a4c4d8512446ce2132263d7816cbc

Rewrite enumerate_range to fix some edge case bugs, fix yet another edge case bug in pfsec_arm64_resolve_adrp_ldr_str_add_reference_auto, add comments to pfsec_find_prev_inst and pfsec_find_next_inst to unambiguously explain their behavior

src/PatchFinder.c
@@ -346,6 +346,10 @@ int pfsec_find_memory(PFSection *section, uint64_t searchStartAddr, uint64_t sea
     return r;
 }
 
+// pfsec_find_prev_inst will start searching at the instruction *before* the instruction at startAddr
+// + 0x1000: [first search comparison]
+// + 0x1004: [startAddr]
+
 uint64_t pfsec_find_prev_inst(PFSection *section, uint64_t startAddr, uint32_t searchCount, uint32_t inst, uint32_t mask)
 {
     uint64_t out = 0;
@@ -354,6 +358,9 @@ uint64_t pfsec_find_prev_inst(PFSection *section, uint64_t startAddr, uint32_t s
     if (!out) return 0;
     return out;
 }
+
+// pfsec_find_next_inst will start searching at the instruction at startAddr
+// + 0x1000: [startAddr, first search comparison]
 
 uint64_t pfsec_find_next_inst(PFSection *section, uint64_t startAddr, uint32_t searchCount, uint32_t inst, uint32_t mask)
 {

src/PatchFinder_arm64.c
@@ -112,7 +112,7 @@ uint64_t pfsec_arm64_resolve_adrp_ldr_str_add_reference_auto(PFSection *section,
 			uint32_t specialCaseAddInst = 0, specialCaseAddMask = 0;
 			arm64_gen_add_imm(reg, reg, OPT_UINT64_NONE, &specialCaseAddInst, &specialCaseAddMask);
 
-			uint64_t specialCaseAddAddr = pfsec_find_prev_inst(section, ldrStrAddAddr - sizeof(uint32_t), searchCount, specialCaseAddInst, specialCaseAddMask);
+			uint64_t specialCaseAddAddr = pfsec_find_prev_inst(section, ldrStrAddAddr, searchCount, specialCaseAddInst, specialCaseAddMask);
 			if (specialCaseAddAddr) {
 				// In this case, we need to make sure there are no writes in the two "(0..n) instructions" sections
 				if (pfsec_arm64_scan_register_write(section, reg, ldrStrAddAddr, specialCaseAddAddr)) return 0;

src/Util.c
@@ -48,24 +48,19 @@ void enumerate_range(uint64_t start, uint64_t end, uint16_t alignment, size_t nb
     if (alignment == 0) return;
     if (nbytes == 0) return;
     if (nbytes % alignment) return;
-    if (nbytes > (end - start)) return;
 
     int dir = start < end ? 1 : -1;
 
-    if (dir == 1) {
-        end -= nbytes;
-        if (start >= end) return;
-    }
-    else {
-        start -= nbytes;
-        if (start <= end) return;
-    }
+    uint64_t absStart = dir == 1 ? start : end;
+    uint64_t absEnd   = dir == 1 ? end : start;
+    uint64_t absSpace = absEnd - absStart;
+    if (alignment > absSpace) return;
+    if (absSpace % alignment) return;
 
-    for (uint64_t cur = start; dir == 1 ? (cur + (alignment * dir)) <= end : (cur + (alignment * dir)) >= end; cur += (dir * alignment)) {
+    uint64_t absSteps = absSpace / alignment;
+    for (uint64_t i = 0; i < absSteps; i++) {
+        uint64_t cur = start + (i * alignment * dir) - (dir == -1 ? alignment : 0);
         if (!enumerator(cur)) break;
-
-        // Extra condition to prevent underflow when we hit 0 and the direction is backwards
-        if (dir == -1 && cur == 0) break;
     }
 }
 

commit b1a4f2debf2aff70edc2825c5cfbd05926d7fc18

Fix weird regression caused by commit 77c2a6d (would cause deadlocks in certain cases when searching backwards using a pattern metric)

src/PatchFinder.c
@@ -451,7 +451,14 @@ void _pfsec_run_pattern_metric(PFSection *section, uint64_t startAddr, uint64_t 
         bool stop = false;
         matchBlock(startAddr, &stop);
         if (stop) break;
-        startAddr += alignment;
+
+        if (endAddr >= startAddr) {
+            startAddr += alignment;
+        }
+        else {
+            // When searching backwards, the search will automatically start one instruction before startAddr
+            // Due to that, we do not need to decrement it by anything
+        }
     }
 }
 